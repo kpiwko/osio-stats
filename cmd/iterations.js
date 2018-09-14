35e029fa8f21f06dea6abb65aea6fa4898a081df
Karel Piwko <kpiwko@redhat.com>
2018-09-14 14:40:19 +0200

    feat: ability to call iterations from API

--- cmd/iterations.js
+++ cmd/iterations.js
@@ -29,95 +29,93 @@ ${datatransformers.all.map(dt => {return '['+dt.id+']\t\t' + chalk.bold(dt.title
     .wrap(null)
 }
 
-/* global console */
-const handler = function(argv) {
+const executor = async function(argv) {
 
   const planner = new Planner()
-
-  planner.iterationsWithDetails(argv.space, argv['include-item-types'])
-    .then(iterations => {
-      
-      // sort iterations
-      iterations = iterations.sort((a, b) => {
-        return a.name.localeCompare(b.name)
-      })
-
-      const columns = datatransformers.all.filter(dt => {
-        return argv.columns.includes(dt.id)
-      })
-
-      // tsv output
-      if(argv.tsv) {
-        const fields = columns.map(column => {
-          return {
-            label: column.title,
-            value: column.id
-          }
-        })
-        const parser = new json2csv.Parser({
-          fields: fields,
-          delimiter: '\t'
-        })
-        console.log(parser.parse(iterations))
-      }
-      // json output
-      else if(argv.json) {
-        console.log(JSON.stringify(iterations, null, 2))
-      }
-      else if(argv.html) {
-        (async () => {
-          const metadata = Object.keys(argv).filter(k => k!=='_' && k!=='$0').map(k => {
-            const value = argv[k]
-            return `--${k} ${Array.isArray(value) ? (value.length ? value.join(' ') : '[]') : value}`
-          }).join(' ')
-          const data = iterationsHelper(iterations, columns)
-          const date = new Date(Date.now()).toUTCString()
-
-          const content = await formatter.html(command, {
-            metadata,
-            data,
-            date,
-            dts: datatransformers.all,
-            title: 'OpenShift.io Iterations Statistics' 
-          })
-          console.log(content)
-        })()
-      }
-      // pretty print output
-      else {
-        const header = columns.map(column => {
-          return {
-            value: column.title
-          }
-        })
-        const table = Table(header, [], {defaultValue: ''})
-        // transform data
-        iterations.forEach(iteration => {
-          const entry = columns.reduce((acc, column) => {
-            acc.push(iteration[column.id])
-            return acc
-          }, [])
-          table.push(entry)
-        })
-        console.log(table.render())
-      }      
-    })
-    .catch(error => {
-      console.log(error)
-      process.exit(1)
-    })
-}
-
-const iterationsHelper = (iterations, columns) => {
-  const data = []
-  iterations.forEach(iteration => {
-    const entry = columns.reduce((acc, column) => {
-      acc.push(iteration[column.id])
-      return acc
-    }, [])
-    data.push(`<td>${entry.join('</td><td>')}</td>`)
+  
+  const columns = datatransformers.all.filter(dt => {
+    return argv.columns.includes(dt.id)
   })
-  return data
+
+  let iterations = await planner.iterationsWithDetails(argv.space, argv['include-item-types'])
+
+  // sort iterations
+  iterations = iterations.sort((a, b) => {
+    return a.name.localeCompare(b.name)
+  })
+
+  if(argv.tsv) {
+    const fields = columns.map(column => {
+      return {
+        label: column.title,
+        value: column.id
+      }
+    })
+    return formatter.tsv(iterations, fields)
+  }
+  else if(argv.json) {
+    return formatter.json(iterations)
+  }
+  else if(argv.html) {
+
+    const iterationsHelper = (iterations, columns) => {
+      const data = []
+      iterations.forEach(iteration => {
+        const entry = columns.reduce((acc, column) => {
+          acc.push(iteration[column.id])
+          return acc
+        }, [])
+        data.push(`<td>${entry.join('</td><td>')}</td>`)
+      })
+      return data
+    }
+
+    const metadata = Object.keys(argv).filter(k => k!=='_' && k!=='$0').map(k => {
+      const value = argv[k]
+      return `--${k} ${Array.isArray(value) ? (value.length ? value.join(' ') : '[]') : value}`
+    }).join(' ')
+    const data = iterationsHelper(iterations, columns)
+    const date = new Date(Date.now()).toUTCString()
+
+    return await formatter.html({
+      metadata,
+      data,
+      date,
+      dts: datatransformers.all,
+      title: 'OpenShift.io Iterations Statistics' 
+    }, command)
+  }
+  else {
+    const header = columns.map(column => {
+      return {
+        value: column.title
+      }
+    })
+    const table = Table(header, [], {defaultValue: ''})
+    // transform data
+    iterations.forEach(iteration => {
+      const entry = columns.reduce((acc, column) => {
+        acc.push(iteration[column.id])
+        return acc
+      }, [])
+      table.push(entry)
+    })
+    return table.render()  
+  }
 }
 
-module.exports = {command, describe, builder, handler}
+/* global console */
+const handler = function(argv) {
+  (async ()=> {
+    try {
+      const output = await executor(argv)
+      console.log(output)
+    }
+    catch(err) {
+      console.log(err)
+      process.exit(1)
+    }
+  })()
+}
+
+module.exports = {command, describe, builder, handler, executor}
--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-#!/usr/bin/env node
-'use strict'
+const Planner = require('./lib/planner')
+const iterations = require('./cmd/iterations')
 
-const yargs = require('yargs')
-
-const cli = function() {
-  yargs
-    .usage('usage: $0 <command>')
-    .commandDir('./cmd')
-    .demandCommand()
-    .option('space', {
-      describe: 'Space (id) to work with',
-      default: 'e8864cfe-f65a-4351-85a4-3a585d801b45',
-      defaultDescription: 'OpenShift_io'
-    })
-    .option('include-item-types', {
-      describe: 'Filter any query by item type(s)',
-      type: 'array',
-      default: [],
-      defaultDescription: 'Include all item types'
-    })
-    .option('tsv', {
-      describe: 'Print out query outcome in Tab Separated Value format',
-      default: false
-    })
-    .option('json', {
-      describe: 'Print out query outcome in JSON format',
-      default: false
-    })
-    .option('html', {
-      describe: 'Print out query outcome is HTML format',
-      default: false
-    })
-    .help('help')
-    .version()
-    //.wrap(null)
-    .argv
+module.exports = {
+  Planner,
+  iterations
 }
-
-cli()
